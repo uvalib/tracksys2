0860e968aba7ebccfa600560c5e0aaa89ca99169
Lou Foster <louffoster@gmail.com>
2022-09-19 15:45:06 -0400

    added order messages

--- frontend/src/views/OrderDetails.vue
+++ frontend/src/views/OrderDetails.vue
@@ -1,40 +1,48 @@
 <template>
    <h2>Order {{route.params.id}}</h2>
    <div class="details" v-if="systemStore.working==false">
-      <Panel header="General Information">
-         <dl>
-            <DataDisplay label="Status" :value="detail.status">
-               <div class="status">
-                  <span :class="`status ${detail.status}`">{{displayStatus(detail.status)}}</span>
-                  <DPGButton icon="pi pi-info-circle" class="p-button-rounded p-button-text"
-                     @click="toggleEvents" aria-haspopup="true" aria-controls="events-panel" />
-               </div>
-            </DataDisplay>
-            <OverlayPanel ref="events" id="events-panel" :showCloseIcon="true">
-               <DataTable :value="ordersStore.events" ref="eventsTable" dataKey="id" :lazy="false"
-                  stripedRows showGridlines responsiveLayout="scroll" class="p-datatable-sm"
-               >
-                  <Column header="Date">
-                     <template #body="slotProps">
-                        {{formatDateTime(slotProps.data.createdAt)}}
-                     </template>
-                  </Column>
-                  <Column header="User">
-                     <template #body="slotProps">
-                        {{slotProps.data.staffMember.firstName}} {{slotProps.data.staffMember.lastName}}
-                     </template>
-                  </Column>
-                  <Column field="details" header="Details" />
-               </DataTable>
-            </OverlayPanel>
-            <DataDisplay v-if="detail.status=='completed'" label="Date Completed" :value="formatDateTime(detail.dateCompleted)"/>
-            <DataDisplay label="Customer" :value="`${detail.customer.lastName}, ${detail.customer.firstName}`"/>
-            <DataDisplay label="Agency" :value="detail.agency.name"/>
-            <DataDisplay label="Title" :value="detail.title"/>
-            <DataDisplay label="Special Instructions" :value="detail.specialInstructions"/>
-            <DataDisplay label="Staff Notes" :value="detail.staffNotes"/>
-         </dl>
-      </Panel>
+      <div class="left">
+         <Panel header="General Information">
+            <dl>
+               <DataDisplay label="Status" :value="detail.status">
+                  <div class="status">
+                     <span :class="`status ${detail.status}`">{{displayStatus(detail.status)}}</span>
+                     <DPGButton icon="pi pi-info-circle" class="p-button-rounded p-button-text"
+                        @click="toggleEvents" aria-haspopup="true" aria-controls="events-panel" />
+                  </div>
+               </DataDisplay>
+               <OverlayPanel ref="events" id="events-panel" :showCloseIcon="true">
+                  <DataTable :value="ordersStore.events" ref="eventsTable" dataKey="id" :lazy="false"
+                     stripedRows showGridlines responsiveLayout="scroll" class="p-datatable-sm"
+                  >
+                     <Column header="Date">
+                        <template #body="slotProps">
+                           {{formatDateTime(slotProps.data.createdAt)}}
+                        </template>
+                     </Column>
+                     <Column header="User">
+                        <template #body="slotProps">
+                           {{slotProps.data.staffMember.firstName}} {{slotProps.data.staffMember.lastName}}
+                        </template>
+                     </Column>
+                     <Column field="details" header="Details" />
+                  </DataTable>
+               </OverlayPanel>
+               <DataDisplay v-if="detail.status=='completed'" label="Date Completed" :value="formatDateTime(detail.dateCompleted)"/>
+               <DataDisplay label="Customer" :value="`${detail.customer.lastName}, ${detail.customer.firstName}`"/>
+               <DataDisplay label="Agency" :value="detail.agency.name"/>
+               <DataDisplay label="Title" :value="detail.title"/>
+               <DataDisplay label="Special Instructions" :value="detail.specialInstructions"/>
+               <DataDisplay label="Staff Notes" :value="detail.staffNotes"/>
+            </dl>
+         </Panel>
+         <Panel header="Messages">
+            <div class="msg" v-if="detail.status== 'requested'">Order is not yet approved.</div>
+            <div class="msg" v-if="detail.status== 'deferred'">Order has been deferred.</div>
+            <div class="msg" v-if="detail.status== 'await_fee'">Order is awaiting customer fee payment.</div>
+            <div class="msg" v-if="detail.customer.academicStatusID==1 && !detail.fee">Either enter a fee, defer or cancel this order.</div>
+         </Panel>
+      </div>
       <Panel header="Workflow">
          <dl>
             <DataDisplay label="Date Submitted" :value="formatDate(detail.dateSubmitted)"/>
@@ -237,6 +245,15 @@ div.item {
       margin-top: -5px;
    }
 
+   div.left {
+      margin: 10px;
+      flex: 45%;
+      text-align: left;
+      .msg {
+         margin: 5px 0;
+         font-size: 0.9em;
+      }
+   }
    :deep(div.p-panel) {
       margin: 10px;
       flex: 45%;
