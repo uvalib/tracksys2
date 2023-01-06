4aae914b12a61272559aa3f8fc34cabdd7ff3fc6
Lou Foster <louffoster@gmail.com>
2023-01-06 08:43:11 -0500

    enable migrate

--- package/scripts/migrate.sh
+++ package/scripts/migrate.sh
@@ -4,8 +4,8 @@
 #
 
 # run the migrations
-#bin/migrate -path db -verbose -database "mysql://$DBUSER:$DBPASS@tcp($DBHOST:$DBPORT)/$DBNAME?x-migrations-table=ts2_schema_migrations" up
-echo "WOULD DO: bin/migrate -path db -verbose -database 'mysql://$DBUSER:$DBPASS@tcp($DBHOST:$DBPORT)/$DBNAME?x-migrations-table=ts2_schema_migrations' up"
+bin/migrate -path db -verbose -database "mysql://$DBUSER:$DBPASS@tcp($DBHOST:$DBPORT)/$DBNAME?x-migrations-table=ts2_schema_migrations" up
+#echo "WOULD DO: bin/migrate -path db -verbose -database 'mysql://$DBUSER:$DBPASS@tcp($DBHOST:$DBPORT)/$DBNAME?x-migrations-table=ts2_schema_migrations' up"
 
 
 # return the status
