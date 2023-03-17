a2c0b7182af0384fbf7cbca2a6685c3d4cbcdf3e
Lou Foster <louffoster@gmail.com>
2023-03-17 14:49:11 -0400

    filtering for orders panel

--- frontend/src/components/related/RelatedOrders.vue
+++ frontend/src/components/related/RelatedOrders.vue
@@ -7,33 +7,68 @@
       :lazy="false" :paginator="props.orders.length > 15" :rows="15" :rowsPerPageOptions="[15,30,50]" removableSort
       paginatorTemplate="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink RowsPerPageDropdown"
       currentPageReportTemplate="{first} - {last} of {totalRecords}"
+      v-model:filters="filters" filterDisplay="menu"
    >
       <Column field="id" header="ID" :sortable="true">
          <template #body="slotProps">
             <router-link :to="`/orders/${slotProps.data.id}`">{{slotProps.data.id}}</router-link>
          </template>
       </Column>
-      <Column  field="customer.lastName" header="Customer" class="nowrap" :sortable="true">
+      <Column  field="customer.lastName" header="Customer" class="nowrap" :sortable="true" filterField="customer.lastName" :showFilterMatchModes="false" >
+         <template #filter="{filterModel}">
+            <InputText type="text" v-model="filterModel.value" placeholder="Last name"/>
+         </template>
          <template #body="slotProps">
             {{slotProps.data.customer.lastName}}, {{slotProps.data.customer.firstName}}
          </template>
       </Column>
-      <Column field="agency.name" header="Agency" class="nowrap" :sortable="true"/>
-      <Column field="title" header="Order Title" />
-      <Column field="notes" header="Staff Notes" />
-      <Column field="specialInstructions" header="Special Instructions" />
+      <Column field="agency.name" header="Agency" class="nowrap" :sortable="true" filterField="agency.id" :showFilterMatchModes="false" >
+         <template #filter="{filterModel}">
+            <Dropdown v-model="filterModel.value" :options="systemStore.agencies" optionLabel="name" optionValue="id" placeholder="Select anegency" />
+         </template>>
+      </Column>
+      <Column field="title" header="Order Title" filterField="title" :showFilterMatchModes="false" >
+         <template #filter="{filterModel}">
+            <InputText type="text" v-model="filterModel.value" placeholder="Title"/>
+         </template>
+      </Column>
+      <Column field="notes" header="Staff Notes" filterField="staffNotes" :showFilterMatchModes="false" >
+         <template #filter="{filterModel}">
+            <InputText type="text" v-model="filterModel.value" placeholder="Staff notes"/>
+         </template>
+      </Column>
+      <Column field="specialInstructions" header="Special Instructions" filterField="specialInstructions" :showFilterMatchModes="false" >
+         <template #filter="{filterModel}">
+            <InputText type="text" v-model="filterModel.value" placeholder="Special instructions"/>
+         </template>
+      </Column>
    </DataTable>
 </template>
 
 <script setup>
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
+import { ref } from 'vue'
+import { FilterMatchMode } from 'primevue/api'
+import Dropdown from 'primevue/dropdown'
+import InputText from 'primevue/inputtext'
+import { useSystemStore } from '@/stores/system'
+
+const systemStore = useSystemStore()
 
 const props = defineProps({
    orders: {
       type: Array,
       required: true
    }
+})
+
+const filters = ref( {
+   'agency.id': {value: null, matchMode: FilterMatchMode.EQUALS},
+   'customer.lastName': {value: null, matchMode: FilterMatchMode.STARTS_WITH},
+   'title': {value: null, matchMode: FilterMatchMode.CONTAINS},
+   'specialInstructions': {value: null, matchMode: FilterMatchMode.CONTAINS},
+   'staffNotes': {value: null, matchMode: FilterMatchMode.CONTAINS},
 })
 </script>
 
