b7989dd78db7a37ba343b6b6ca428460ddd12380
Lou Foster <louffoster@gmail.com>
2023-12-15 13:53:22 -0500

    added as review searching

--- frontend/src/views/ArchivesSpaceReviews.vue
+++ frontend/src/views/ArchivesSpaceReviews.vue
@@ -1,14 +1,25 @@
 <template>
    <h2>ArchivesSpace Reviews</h2>
    <div class="reviews">
-      <DataTable :value="archivesSpace.reviews" ref="apTrustStatusTable" dataKey="id"
+      <DataTable :value="archivesSpace.reviews" ref="asReviewsTable" dataKey="id"
             stripedRows showGridlines responsiveLayout="scroll" class="p-datatable-sm" :lazy="false"
             :resizableColumns="true" columnResizeMode="fit" paginatorPosition="top"
             :paginator="true" :rows="15" :rowsPerPageOptions="[15,30,50]" removableSort
             :sortField="archivesSpace.searchOpts.sortField" :sortOrder="sortOrder"
             paginatorTemplate="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink RowsPerPageDropdown"
             currentPageReportTemplate="{first} - {last} of {totalRecords}"
+            v-model:filters="filter"
+            :globalFilterFields="['metadata.title', 'submitter.lastName']"
          >
+         <template #paginatorstart >
+         </template>
+         <template #paginatorend>
+            <span class="js-search p-input-icon-right">
+               <i class="pi pi-search" />
+               <InputText v-model="filter['global'].value" placeholder="Search reviews..."/>
+            </span>
+            <DPGButton label="Clear" class="p-button-secondary clear" @click="clearSearch()"/>
+         </template>
          <Column field="metadata.pid" header="PID"  class="nowrap" :sortable="true">
             <template #body="slotProps">
                <router-link :to="`/metadata/${slotProps.data.metadata.id}`">
@@ -45,6 +56,8 @@
 import { onMounted, ref, computed } from 'vue'
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
+import InputText from 'primevue/inputtext'
+import { FilterMatchMode } from 'primevue/api'
 import { usePinnable } from '@/composables/pin'
 import { useArchivesSpaceStore } from '@/stores/archivesspace'
 import dayjs from 'dayjs'
@@ -52,6 +65,7 @@ import dayjs from 'dayjs'
 usePinnable("p-paginator-top")
 
 const archivesSpace = useArchivesSpaceStore()
+const filter = ref( {'global': {value: null, matchMode: FilterMatchMode.CONTAINS}})
 
 onMounted(() => {
    archivesSpace.getReviews()
@@ -71,6 +85,10 @@ const formatDate = (  (date ) => {
    return ""
 })
 
+const clearSearch = (() => {
+   filter.value['global'].value = ""
+})
+
 </script>
 
 <style scoped lang="scss">
@@ -78,5 +96,8 @@ const formatDate = (  (date ) => {
    min-height: 600px;
    text-align: left;
    padding: 0 25px;
+   button.clear {
+      margin-left: 10px;
+   }
 }
 </style>
