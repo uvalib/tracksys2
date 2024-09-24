b67caebd263a709721e153775e61c290bcc82273
Lou Foster <louffoster@gmail.com>
2024-09-24 13:14:17 -0400

    update formkit and fix plugins

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,57 +1,68 @@
- /*global process */
+/*global process */
 
- import { fileURLToPath, URL } from 'url'
- import { defineConfig } from 'vite'
- import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
- export default defineConfig({
+const hash = Math.floor(Math.random() * 90000) + 10000
+
+export default defineConfig({
    define: {
       // enable hydration mismatch details in production build
       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
-    },
-    plugins: [vue()],
-    resolve: {
-       alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url))
-       }
-    },
-    server: { // this is used in dev mode only
-       port: 8080,
-       proxy: {
-          '/api': {
-             target: process.env.TRACKSYS2_SRV,  //export TRACKSYS2_SRV=http://localhost:8085
-             changeOrigin: true
-          },
-          '/authenticate': {
-             target: process.env.TRACKSYS2_SRV,
-             changeOrigin: true
-          },
-          '/cleanup': {
+   },
+   plugins: [vue()],
+   resolve: {
+      alias: {
+         '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+   },
+   build: {
+      rollupOptions: {
+         output: {
+            entryFileNames: `[name]` + hash + `.js`,
+            chunkFileNames: `[name]` + hash + `.js`,
+            assetFileNames: `[name]` + hash + `.[ext]`
+         }
+      }
+   },
+   server: { // this is used in dev mode only
+      port: 8080,
+      proxy: {
+         '/api': {
+            target: process.env.TRACKSYS2_SRV,  //export TRACKSYS2_SRV=http://localhost:8085
+            changeOrigin: true
+         },
+         '/authenticate': {
             target: process.env.TRACKSYS2_SRV,
             changeOrigin: true
          },
-          '/config': {
+         '/cleanup': {
             target: process.env.TRACKSYS2_SRV,
             changeOrigin: true
          },
-          '/healthcheck': {
-             target: process.env.TRACKSYS2_SRV,
-             changeOrigin: true
-          },
-          '/pdf': {
-             target: process.env.TRACKSYS2_SRV,
-             changeOrigin: true
-          },
-          '/upload_search_image': {
+         '/config': {
             target: process.env.TRACKSYS2_SRV,
             changeOrigin: true
          },
-          '/version': {
-             target: process.env.TRACKSYS2_SRV,
-             changeOrigin: true
-          },
-       }
-    },
- })
+         '/healthcheck': {
+            target: process.env.TRACKSYS2_SRV,
+            changeOrigin: true
+         },
+         '/pdf': {
+            target: process.env.TRACKSYS2_SRV,
+            changeOrigin: true
+         },
+         '/upload_search_image': {
+            target: process.env.TRACKSYS2_SRV,
+            changeOrigin: true
+         },
+         '/version': {
+            target: process.env.TRACKSYS2_SRV,
+            changeOrigin: true
+         },
+      }
+   },
+})
 
 
