7f751eb9863bcbf79d23de18189fe2b16223e49b
Lou Foster <louffoster@gmail.com>
2025-05-27 16:34:43 -0400

    fix typo

--- frontend/src/components/SearchHelpPopover.vue
+++ frontend/src/components/SearchHelpPopover.vue
@@ -7,7 +7,7 @@
          <li>Select an item type to constrain the search to a single type.</li>
          <li>All search terms are stemmed, meaning that a search for <span class="example">prove</span> will match proved, proven and proving.</li>
          <li>If multiple search terms are specifed, records that contain ALL terms in any order (not necessarily adjacent) will be returned.</li>
-         <li>Separate multiple search terms with <span class="example">|</span> to return records that match match any of the terms.</li>
+         <li>Separate multiple search terms with <span class="example">|</span> to return records that match any of the terms.</li>
          <li>Enclose search terms in quotes to find exact matches.<br/>Example: <span class="example">"julian bond papers"</span></li>
          <li>Add <span class="example">*</span> for a wildcard search.<br/>Example: <span class="example">scatter*</span> will match Scattergood and scattered.</li>
          <li>Add <span class="example">?</span> to match any single character. <br/>Example: <span class="example">f?st</span> will match FAST, but not FEAST.</li>
