86f8ddd1e78dfeb6fe3b196492d4a31a16e01e6a
Lou Foster <louffoster@gmail.com>
2026-02-11 16:37:53 -0500

    fix logic for showing generate / regenerate deliverables

--- frontend/src/views/UnitDetails.vue
+++ frontend/src/views/UnitDetails.vue
@@ -65,10 +65,10 @@
             </div>
             <div class="acts">
                <template v-if="detail.intendedUseID != 110">
-                  <DPGButton v-if="detail.datePatronDeliverablesReady == null && detail.status =='done'" @click="generateDeliverablesClicked"
+                  <DPGButton v-if="detail.datePatronDeliverablesReady == null && unitsStore.hasMasterFiles" @click="generateDeliverablesClicked"
                      severity="secondary" label="Generate Deliverables"
                   />
-                  <DPGButton v-if="unitsStore.hasMasterFiles && detail.status =='done'" @click="generateDeliverablesClicked" severity="secondary" label="Regenerate Deliverables" />
+                  <DPGButton v-else @click="generateDeliverablesClicked" severity="secondary" label="Regenerate Deliverables" />
                </template>
                <DPGButton v-if="unitsStore.canDownload" @click="downloadClicked" severity="secondary" label="Download from Archive" />
                <DPGButton label="OCR Master Files" severity="secondary"  @click="unitOCRClicked()" v-if="unitsStore.canOCR && (userStore.isAdmin || userStore.isSupervisor)" />
