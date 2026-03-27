d1f51f869de7a458fe13ac8edb997ce805d8df8e
Lou Foster <louffoster@gmail.com>
2026-03-27 16:10:54 -0400

    simplify add attachment. no form needed

--- frontend/src/components/unit/AddAttachmentDialog.vue
+++ frontend/src/components/unit/AddAttachmentDialog.vue
@@ -1,14 +1,16 @@
 <template>
    <DPGButton @click="show" severity="secondary" label="Add Attachment"/>
    <Dialog v-model:visible="isOpen" :modal="true" header="Add Attachment" :style="{width: '400px'}" :closable="false">
-      <FormKit type="form" id="add-attachment" :actions="false" @submit="addAttachment">
-         <FormKit label="" type="file" v-model="info.attachment" required outer-class="first" />
-         <FormKit label="Brief Description" type="textarea" rows="4" v-model="info.description"/>
-         <div class="acts">
-            <DPGButton @click="hide" label="Cancel" severity="secondary"/>
-            <FormKit type="submit" label="Attach File" wrapper-class="submit-button" />
-         </div>
-      </FormKit>
+      <div class="upload">
+         <FileUpload ref="uploader" mode="basic" :customUpload="true" name="file" @uploader="startUpload($event)" />
+         <FormField id="attdesc" label="Brief Description">
+            <Textarea id="attdesc" v-model="description" rows="4"/>   
+         </FormField>
+      </div>
+      <template #footer>
+         <DPGButton @click="hide" label="Cancel" severity="secondary"/>
+         <DPGButton @click="uploader.upload()" label="Attach File" />
+      </template>
    </Dialog>
 </template>
 
@@ -16,37 +18,53 @@
 import { ref } from 'vue'
 import Dialog from 'primevue/dialog'
 import { useUnitsStore } from '@/stores/units'
+import { useSystemStore } from '@/stores/system'
+import FileUpload from 'primevue/fileupload'
+import Textarea from 'primevue/textarea'
+import FormField from '@/components/FormField.vue'
+import axios from 'axios'
 
-const unitsStore = useUnitsStore()
+const unit = useUnitsStore()
+const system = useSystemStore()
 
 const isOpen = ref(false)
-const info = ref({
-   attachment: null,
-   description: ""
+const uploader = ref()
+const description = ref("")
+
+const startUpload = ( (event) => {
+   const fileData = event.files[0]
+   let formData = new FormData()
+   formData.append("description", description.value)
+   formData.append("name", fileData.name)
+   formData.append("file", fileData)
+   const url=`${system.jobsURL}/units/${unit.detail.id}/attach`
+   axios.post(url, formData, {
+      headers: {
+         'Content-Type': 'multipart/form-data',
+      }
+   }).then(async () => {
+      let uID = unit.detail.id
+      unit.detail.id = 0
+      await unit.getDetails(uID)
+      hide()
+   }).catch((error) => {
+      system.setError(error)
+   })
 })
 
-async function addAttachment() {
-   await unitsStore.attachFile(info.value)
-   hide()
-}
-function hide() {
+const hide = (() => {
    isOpen.value=false
-}
-function show() {
-   info.value.description = ""
-   info.value.attachment = null
+})
+const show = (() =>{
+   description.value = ""
    isOpen.value = true
-
-}
+})
 </script>
 
 <style lang="scss" scoped>
-.acts {
+.upload {
    display: flex;
-   flex-flow: row nowrap;
-   justify-content: flex-end;
-   padding: 15px 0 10px 0;
-   margin: 0;
-   gap: 10px;
+   flex-direction: column;
+   gap: 15px;
 }
 </style>
